74c1cb449d3a95f125312932565f44f5c583c083
XXieJY <309092483@qq.com>
2017-09-14 23:26:15 +0800

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,69 +1,44 @@
-// 代码实现了： 原地去重算法unique， 这个算法经常会用到。
-/**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
-
-#include "stdafx.h"
-#include <vector>
-#include <iostream>
-#include <algorithm>
-
-using namespace std;
-
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
-
-};
-
 class Solution {
 private:
-
-
-
 	void swap(ListNode* f, ListNode* a) {
 		int tmp = f->val;
 		f->val = a->val;
 		a->val = tmp;
 	}
-
 	void quickSort(ListNode& b, ListNode& e, ListNode* l, ListNode* h) {
 		if (l != h) {
 			ListNode* i = NULL;
 			partition(b, e, l, h, i);
 			ListNode* watch = i, *b_ad = &b, *e_ad = &e;
+			while(b_ad->next != l) {
+				b_ad=b_ad->next;
+			}
 			if (watch == b_ad) {
 				watch = watch->next->next;
 				quickSort(b, e, watch, h);
 			}
 			else if (watch->next->next != e_ad) {
-				quickSort(b, *(i->next), l, i);
+				quickSort(b, *(i->next), l, watch);
 				quickSort(b, e, watch->next->next, h);
 			}
 			else {
 				quickSort(b, *h, l, i);
 			}
-
+			
 		}
-	}
-
+	}	
 	void partition(ListNode& b, ListNode& e, ListNode* l, ListNode* h, ListNode* &i) {
 		int pivot = h->val;
 		i = &b;
 		while (i->next != l) { //locate the place where before the start sorting point
 			i = i->next;
 		}
-		ListNode* t = l;
+		ListNode* t = (ListNode*) malloc(sizeof(ListNode));
+		t = l;
 		while (t != h && t != NULL) {
 			if (t->val < pivot) {
 				i = i->next;
-				swap(i, l);
+				swap(i, t);
 			}
 			else {
 				// donothing;
@@ -72,8 +47,8 @@ private:
 		}
 		swap(i->next, h);
 	}
-
-
+	
+	
 public:
 	ListNode* sortList(ListNode* head) {
 		if (head == NULL) return head;  //for empty and single element LinkedList
@@ -94,17 +69,3 @@ public:
 		return head;
 	}
 };
-
-
-int main() {
-	ListNode a = 3;
-	ListNode b = 4;
-	ListNode c = 1;
-	ListNode* head = &a;
-	a.next = &b;
-	b.next = &c;
-	Solution s;
-	s.sortList(head);
-	cout << head->val << ', ' << head->next->val;
-	return 1;
-}
